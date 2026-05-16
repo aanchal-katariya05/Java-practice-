e7bd5287cc0505555725bd579d7f8899a7bf6c80
Aanchal Katariya <2005katariyaaanchal@gmail.com>
2026-05-16 23:21:34 +0530

    another one

---
+++ Array/LargestNumber.java
@@ -0,0 +1,21 @@
+
+
+public class LargestNumber {
+
+    public static void main(String[] args) {
+
+        int arr[] = {12, 45, 7, 89, 23};
+
+        int largest = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+
+            if(arr[i] > largest) {
+
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest number is : " + largest);
+    }
+}
